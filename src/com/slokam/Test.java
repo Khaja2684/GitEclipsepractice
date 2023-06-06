1068b9559207848a6938de0b7a1bdd90e6080457
khaja <khajashaik7365@gmail.com>
2023-06-06 12:29:52 +0530

    first commit

---
+++ src/com/slokam/Test.java
@@ -0,0 +1,17 @@
+package com.slokam;
+
+public class Test {
+
+	public static void main(String[] args) {
+       
+		
+		String k = "rubeena";
+		
+		for(int i = 0; i<k.length(); i++)
+		{
+			System.out.println(k);
+		}
+
+	}
+
+}
